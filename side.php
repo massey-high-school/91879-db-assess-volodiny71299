c390ad3d3b76e0b26072134d14c23d0697b64e44
Yara <58008773+volodiny71299@users.noreply.github.com>
2020-09-25 10:55:42 +1200

    :nose: :keycap_ten:

--- side.php
+++ side.php
@@ -14,8 +14,15 @@
                 <input class="submit" type="submit" name="find_name" value="&#128269;"/>
                 
             </form>
+            <!-- End of name search -->
             
-            
+            <form method="post" action="name-search.php" enctype="multipart/form-data" >
+    
+                <input class="search" type="text" name="name" size="40" value="" required placeholder="Dish name..." />
+                
+                <input class="submit" type="submit" name="find_name" value="&#128269;"/>
+                
+            </form>
             
             
             
